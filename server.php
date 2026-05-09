daa4fb4a479fbb7cbf57eaf8d44f0f51f912d032
Ubuntu <ubuntu@ip-172-31-7-129.ap-northeast-1.compute.internal>
2026-05-09 10:31:55 +0000

    server.phpを書いた

---
+++ server.php
@@ -0,0 +1,7 @@
+<?php
+
+function h(string $s) : string{
+  return htmlspecialchars($s, ENT_QUOTES);
+}
+echo "ブラウザ（user agent）は" . h($_SERVER['HTTP_USER_AGENT'] . "です<br>\n");
+echo "接続元IPは" . h($_SERVER['REMOTE_ADDR']) . "です<br>\n";
