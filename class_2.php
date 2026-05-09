b7dd4f28831da238117aaf7de49bc15e57ecec6c
Ubuntu <ubuntu@ip-172-31-7-129.ap-northeast-1.compute.internal>
2026-05-09 13:42:57 +0000

    class_2.phpを書いた

---
+++ class_2.php
@@ -0,0 +1,19 @@
+<?php
+
+class MyClass
+{
+  const HOGE = 'string';
+
+  public function __construct()
+  {
+    echo "コンストラクタが動いた！！\n";
+  }
+
+  public function __destruct()
+  {
+    echo "デストラクタが動いた！！\n";
+  }
+}
+
+var_dump( MyClass::HOGE );
+$obj = new MyClass;
